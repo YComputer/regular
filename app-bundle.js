944989f714691b41b4243097f4dbae94ff15aeb4
YComputer <thea@163.com>
2016-09-08 21:09:50 +0800

    exercise2

--- app-bundle.js
+++ app-bundle.js
@@ -44,16 +44,30 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	
+	// var str = `Is this This`;
+	// var regex = /is/gi;
+
+	// var str = `Cat sat on the hat.`;
+	// var regex = /\./g;
+
+	var str = `foo
+	foobar
+	foobaz
+	fooboo`;
+	var regex = /foo/g
+
+
 	var output = (str, regex, target) => {
 	    target.innerHTML = str.replace(regex, str => `<span>${str}</span>`);
 	}
-
-	var str = `Is this This`;
-
-	var regex = /is/gi;
-
 	output(str, regex, document.querySelector('pre'));
+
+
+	function foo(n) {
+	    　　　　return function (i) {
+	        　　　　　　return n += i
+	    }
+	}
 
 /***/ }
 /******/ ]);
--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+// var str = `Is this This`;
+// var regex = /is/gi;
+
+// var str = `Cat sat on the hat.`;
+// var regex = /\./g;
+
+var str = `foo
+foobar
+foobaz
+fooboo`;
+var regex = /foo/g
+
 
 var output = (str, regex, target) => {
     target.innerHTML = str.replace(regex, str => `<span>${str}</span>`);
 }
-
-var str = `Is this This`;
-
-var regex = /is/gi;
-
 output(str, regex, document.querySelector('pre'));
+
+
+function foo(n) {
+    　　　　return function (i) {
+        　　　　　　return n += i
+    }
+}
